198a95ba467c8de284cb4064fd54794675859c70
JesuisDv <jdfrancov2020@gmail.com>
2025-05-24 16:53:35 -0500

    Agregar evento preventivo en la función de búsqueda y mejorar la presentación de la información de la película

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
-async function buscarPelicula() {
+async function buscarPelicula(event) {
+    event.preventDefault()
     nombrePeli = document.getElementById("inputBuscar").value;
     resultPeli = document.querySelector(".pelicula");
 
@@ -13,9 +14,20 @@ async function buscarPelicula() {
         const data = await respuesta.json()
 
         resultPeli.innerHTML = `
-            <img src="${data.image.medium}" alt="">
+        <div>
+            <img src="${data.image.medium}" alt="" class="imgPeli">
+        </div>
+        <div class="infoPeli">
             <p class="namePeli">${data.name}</p>
-            <p class="año">${data.premiered}</p>
+            <p class="año"><strong>Estreno:</strong> ${data.premiered}</p>
+            <p class="genero"><strong>Genero:</strong> ${data.genres}</p>
+            <p class="idioma"><strong>Idioma:</strong> ${data.language}</p>
+            <p class="tipo"><strong>Tipo:</strong> ${data.type}</p>
+            <p class="rate"><strong>Rating:</strong> ${data.rating.average}⭐</p>
+        </div>
+        <div class="sinopsisPeli">
+            <p class="resumen"><strong>Sinopsis:</strong> <br><br> ${data.summary}</p>
+        </div>
         `
     }catch(error) {
         console.error("Error: ", error)
